cce023ba88f2b5dc670737e5148ea251f8ea6da7
Ilya Zelenkevich <ilya.zeliankevich@gmail.com>
2018-08-31 13:44:31 +0300

    init commit

---
+++ src/main/java/com/example/cityDistant/CityDistantApplication.java
@@ -0,0 +1,12 @@
+package com.example.cityDistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityDistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityDistantApplication.class, args);
+	}
+}
