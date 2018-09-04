44ae139ba5ad3bb8d976cb0bc32954f7fb597204
Ilya Zelenkevich <ilya.zeliankevich@gmail.com>
2018-09-04 19:03:04 +0300

    switch to in-memory neo4j for prod

--- src/main/java/com/example/city/distance/config/Neo4jConfig.java
+++ src/main/java/com/example/city/distance/config/Neo4jConfig.java
@@ -1,7 +1,6 @@
 package com.example.city.distance.config;
 
 import org.neo4j.ogm.session.SessionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,23 +10,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableNeo4jRepositories("com.example.city.distance.repository")
-@Profile({"dev", "prod"})
+@Profile({"dev"})
 public class Neo4jConfig {
-
-    @Value("${spring.data.neo4j.uri}")
-    private String databaseUrl;
-
-    @Value("${spring.data.neo4j.username}")
-    private String userName;
-
-    @Value("${spring.data.neo4j.password}")
-    private String password;
 
     @Bean
     public org.neo4j.ogm.config.Configuration getConfiguration() {
         return new org.neo4j.ogm.config.Configuration.Builder()
-                .uri(databaseUrl)
-                .credentials(userName, password)
+                .uri("bolt://localhost")
+                .credentials("neo4j", "password")
                 .build();
     }
 
@@ -35,6 +25,7 @@ public class Neo4jConfig {
     public SessionFactory sessionFactory() {
         return new SessionFactory(getConfiguration(), "com.example.city.distance");
     }
+
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new Neo4jTransactionManager(sessionFactory());
